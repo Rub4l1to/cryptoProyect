2160e4d1bfc3c87ed9561d333d967b60aae14019
Rub4lito <adrian.perez.garcia2018@gmail.com>
2021-02-10 23:37:06 +0100

    Se comienza responsive
    Titulacion responsive realizadas

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,18 @@
 import { typed } from "./utils/index";
 import { drawCards } from "./functions/index";
 import { getList } from "./querys/index";
+import SlimSelect from "slim-select";
 
 const url =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=8&page=1&sparkline=false";
 
-export async function main() {}
+export async function main() {
+  
+}
 
 // Events
 window.onload = async () => {
-  drawCards(await getList(url));
-
+  // drawCards(await getList(url));
   typed(await getList(url));
+  new SlimSelect({ select: "#country" });
 };
