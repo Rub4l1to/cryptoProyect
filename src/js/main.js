607a2753661067303d8dfcfcc71e431a2871c072
Javi García <javigf1992@gmail.com>
2021-02-11 18:43:36 +0100

    new_chart
    Add D3 file.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,13 @@
-export function main() {
+import { typed } from "./utils/index";
+import { drawCards } from "./functions/index";
+import { getList } from "./querys/index";
+import { call, LineGraph } from "./d3/index";
 
 
+
+
+export async function main() {
+  call()
+  LineGraph()
 }
 
