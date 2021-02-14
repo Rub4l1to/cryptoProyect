0253d07df8d17ce571334fe79d5276f716185c72
Rub4lito <adrian.perez.garcia2018@gmail.com>
2021-02-14 08:11:04 +0100

    Archivos para web

--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-import { main } from './main'
-import '../css/main.scss'
-
-main()
-
--- src/js/querys/index.js
+++
@@ -1,7 +0,0 @@
-// Recibe cualquier url y nos devuelve un json con los datos
-const getList = async (url) => {
-  let data = await fetch(url);
-  return data.json();
-};
-
-export { getList };
